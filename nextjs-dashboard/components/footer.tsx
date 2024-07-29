fd484b3cf8f224e826198cc719d0559ec5c096ca
Alexey <alexey.kisilier@gmail.com>
2024-07-29 21:22:34 +0200

    Footer Update v2 - Mobile Optimierung

--- nextjs-dashboard/components/footer.tsx
+++ nextjs-dashboard/components/footer.tsx
@@ -3,12 +3,12 @@ import styles from './Footer.module.css';
 export default function Footer() {
     return (
         <footer>
-            <div>
+            <div className={styles.footerlinks}>
                 <h3><a href="/kontakt">Kontakt</a></h3>
                 <h3><a href="/datenschutz">Datenschutz</a></h3>
                 <h3><a href="/impressum">Impressum</a></h3>
             </div>
-            <div>© Copyright 2024</div>
+            <div className={styles.copyright}>© Copyright 2024</div>
         </footer>
     );
 }
