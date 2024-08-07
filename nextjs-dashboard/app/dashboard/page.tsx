724848ad5d72e892aeb2794f37c48004035f9cc4
Alexey <alexey.kisilier@gmail.com>
2024-08-07 17:14:46 +0200

    Loginseite erstellt (einloggen funktioniert natürlich noch nicht). Hinzufügen des Background. Hinzufügen der neuen Struktur für das Projekt.

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,8 +1,14 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
+
 export default function Page() {
     return (
-    <>
-        <h1>HEADLINE!!</h1>
-        <p>Dashboard Page</p>
-    </>
+        <main className="mainContent">
+            <h1>Das ist das Dashboard</h1>
+        </main>
     );
 }
+
+
+
